bcdb8f7c41be821b195926d8a689aea9b1018704
Karthik Parthiban <karthikdav@gmail.com>
2021-12-06 09:55:18 +0530

    added descriptions for test cases

--- unit.py
+++ unit.py
@@ -4,6 +4,8 @@ import dblayer
 
 class MyTestCase(unittest.TestCase):
     def test_a_truncate_db(self):
+        print("Currently Executing {}".format(self._testMethodName))
+        print("To clear all the records before test cases start")
         try:
             response = dblayer.truncate_table()
             if response == 1:
@@ -15,6 +17,8 @@ class MyTestCase(unittest.TestCase):
             assert False
 
     def test_b_insert_record(self):
+        print("Currently Executing {}".format(self._testMethodName))
+        print("Inserting first record")
         try:
             request_url = 'http://test.com/safe'
             response = dblayer.insert_record(request_url)
@@ -27,6 +31,8 @@ class MyTestCase(unittest.TestCase):
             assert False
 
     def test_c_update_record(self):
+        print("Currently Executing {}".format(self._testMethodName))
+        print("Updating the first record")
         try:
             request_url = 'http://test.com/notsafe'
             response = dblayer.update_record(1, request_url, True)
@@ -39,6 +45,8 @@ class MyTestCase(unittest.TestCase):
             assert False
 
     def test_d_insert_second_record(self):
+        print("Currently Executing {}".format(self._testMethodName))
+        print("Inserting second record")
         try:
             request_url = 'http://10.10.10.10:9200/isclean'
             response = dblayer.insert_record(request_url)
@@ -51,6 +59,8 @@ class MyTestCase(unittest.TestCase):
             assert False
 
     def test_e_read_first_record(self):
+        print("Currently Executing {}".format(self._testMethodName))
+        print("Reading one record")
         try:
             response = dblayer.get_record_by_id(1)
             if len(response) > 2:
@@ -62,6 +72,8 @@ class MyTestCase(unittest.TestCase):
             assert False
 
     def test_f_read_all_record(self):
+        print("Currently Executing {}".format(self._testMethodName))
+        print("Reading all record")
         try:
             response = dblayer.get_all_records()
             if len(response) > 2:
@@ -73,6 +85,8 @@ class MyTestCase(unittest.TestCase):
             assert False
 
     def test_g_delete_record(self):
+        print("Currently Executing ".format(self._testMethodName))
+        print("Deleting a record")
         try:
             response = dblayer.update_record(2, '', False)
             if response == 1:
@@ -84,6 +98,8 @@ class MyTestCase(unittest.TestCase):
             assert False
 
     def test_h_truncate_row(self):
+        print("Currently Executing Cleanup")
+        print("Cleaning the DB after test cases are finished")
         try:
             response = dblayer.truncate_table()
             if response == 1:
